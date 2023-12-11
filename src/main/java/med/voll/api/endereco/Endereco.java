d2257de18e857eb1f44cddbd18959076970f9d16
romulohub <65741659+romulohub@users.noreply.github.com>
2023-12-11 14:03:58 -0300

    Finalização de PUT e DELETE

--- src/main/java/med/voll/api/endereco/Endereco.java
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -28,4 +28,35 @@ public class Endereco {
         this.complemento = dados.complemento();
         this.numero = dados.numero();
     }
+
+    public void atualizarInformacoes(DadosEndereco dados) {
+        if (dados.logradouro() != null) {
+            this.logradouro = dados.logradouro();
+        }
+
+        if (dados.cep() != null) {
+            this.cep = dados.cep();
+        }
+
+        if (dados.bairro() != null) {
+            this.bairro = dados.bairro();
+        }
+
+        if (dados.uf() != null) {
+            this.uf = dados.uf();
+        }
+
+        if (dados.cidade() != null) {
+            this.cidade = dados.cidade();
+        }
+
+        if (dados.complemento() != null) {
+            this.complemento = dados.complemento();
+        }
+
+        if (dados.numero() != null) {
+            this.numero = dados.numero();
+        }
+
+    }
 }
